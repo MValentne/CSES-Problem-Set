d1955885ccfea2746cba03c514b9c36070368f2e
MValentne <valentinomende@gmail.com>
2026-08-04 00:30:31 -0300

    Weird-Algorithm

--- Introductory-Problems/Weird-Algorithm/main.c
+++ Introductory-Problems/Weird-Algorithm/main.c
@@ -1,22 +1,28 @@
+/*
+ * Uso esta consigna como excusa para probar la plataforma
+ * Tambien se prueba con los tests y el script cses-test
+ * Se estudia la forma de evaluacion de estos tests
+ */
+
 #include <stdio.h>
 
 int main(){
-    int n;
-    printf("enter a starting value: ");
-    scanf("%d", &n);
-    if ((n <=1) || (n>= 10000000)){
-        printf("Out of bounds.\n");
-        return(1);
+    //Lo primero es definir una entrada directa para el archivo .in
+    long long n;
+    if (scanf("%lld", &n) != 1) {
+        return 0;
     }
 
     while(n != 1){
-        printf("%d ", n);
+        printf("%lld ", n);
         if(n%2 == 0) {
             n = n/2;
         } else {
             n = (n*3)+1;
         }
     }
-    printf("%d\n", n);
+
+    //Revisar los caracteres de salida adecuados en el .out
+    printf("%lld \n", n);
     return 0;
 }
