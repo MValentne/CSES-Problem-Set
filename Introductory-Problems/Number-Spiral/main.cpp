1d06071a01f60107f8085dbc5e7453235295c009
valentino <valentinomende@gmail.com>
2026-08-22 17:42:30 -0300

    Number-Spiral

--- Introductory-Problems/Number-Spiral/main.cpp
+++ Introductory-Problems/Number-Spiral/main.cpp
@@ -1,73 +1,43 @@
 #include <bits/stdc++.h>
-
+typedef long long ll;
 using namespace std;
 
-int main(){
+ll anillo(ll n){
+    //formula para la diagonal principal de la matriz.
+    return n*n-n+1;
+}
 
+int main(){
     int t;
     cin >> t;
 
-    //guardo las entradas en pares
-    long long x,y;
-    vector <pair<long long, long long>> v, vsorted;
-    for(int i = 0; i < t; i++){
-        scanf("%lld %lld", &y ,&x);
-        v.push_back({y,x});
+    //stack de entradas
+    vector <pair<ll,ll>> in;
+    ll x,y;
+    for(int i = 0; i<t; i++){
+        cin >> y >> x;
+        in.push_back({y,x});
     }
 
-    //las ordeno de menor a mayor.
-    vsorted = v;
-    sort(vsorted.begin(), vsorted.end());
-
-    //creo la tabla segun el valor mas alto.
-    long long highest = max((long long)vsorted.back().first, (long long)vsorted.back().second);
-    //hago la tabla con dimensiones highest x highest.
-
-    // forzado a O(n^2)
-    long long matrix[highest][highest];
-    matrix[0][0] = 1;
-    long long counter = 1;
-    long long incremental = 1;
-    int i = 0;
-    int j = 0;
-    int paridad = 1;
-    while(i < highest && j < highest){
-        if(paridad % 2 == 1){
-            i ++;
-            counter ++;
-            matrix[i][j] = counter;
-            for(int n = 0; n < incremental; n++){
-                j ++;
-                counter ++;
-                matrix[i][j] = counter;
+    // &p vs p: &p referencia al real, p lo copia. (esfuerzo innecesario)
+    //calculo y muestro coordenadas
+    for(auto &p: in){
+        if(max(p.first, p.second) % 2 == 0){
+            if(p.first > p.second){
+                cout << anillo(max(p.first,p.second)) + max(p.first,p.second) - min(p.first,p.second) << "\n";
+            } else if(p.first < p.second){
+                cout << anillo(max(p.first,p.second)) - max(p.first,p.second) + min(p.first,p.second) << "\n";
             }
-            for(int n = 0; n < incremental; n++){
-                i --;
-                counter ++;
-                matrix[i][j] = counter;
-            }
-        } else {
-            j ++;
-            counter ++;
-            matrix[i][j] = counter;
-            for(int n = 0; n < incremental; n++){
-                i ++;
-                counter ++;
-                matrix[i][j] = counter;
-            }
-            for(int n = 0; n < incremental; n++){
-                j --;
-                counter ++;
-                matrix[i][j] = counter;
+        } else{
+            if(p.first > p.second){
+                cout << anillo(max(p.first,p.second)) - max(p.first,p.second) + min(p.first,p.second) << "\n";
+            } else if(p.first < p.second){
+                cout << anillo(max(p.first,p.second)) + max(p.first,p.second) - min(p.first,p.second) << "\n";
             }
         }
-        paridad ++;
-        incremental ++;
-    }
-
-    //Empezamos a resolver las salidas segun el orden de entrada
-    for(auto n:v){
-        cout << matrix[n.second][n.first] << "\n";
+        if(p.first == p.second){
+            cout << anillo(max(p.first, p.second)) << "\n";
+        }
     }
     return 0;
 }
